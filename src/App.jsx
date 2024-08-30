90292b65f3fa1278e5469bc067019414b8adfe9a
Dipszzz <nadhifhisyam500@gmail.com>
2024-08-30 05:14:16 -0700

    feat(workflow): init

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </header>
 
         {/* Main Content */}
-        <main className="flex-grow p-6 overflow-y-auto">
+        <main id="main" className="flex-grow p-6 overflow-y-auto">
           <div className="max-w-4xl mx-auto">
             <h2 className="text-2xl font-semibold mb-4">
               Wellcome to the DipsTech.
@@ -135,7 +135,7 @@ function App() {
               transforming how we interact with technology.
             </p>
             <button
-              onClick={() => (window.location.reload())}
+              onClick={() => (window.location.href = "#main")}
               className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-2 rounded-lg focus:outline-none"
             >
               Back to Home
